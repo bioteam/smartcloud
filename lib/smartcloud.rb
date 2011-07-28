74e4f12a4ace5b216df77c85fd9e63bb91e6b739
Adam Kraut <adamnkraut@gmail.com>
2011-07-27 21:41:11 -0400

    refactor

--- lib/smartcloud.rb
+++ lib/smartcloud.rb
@@ -5,76 +5,57 @@ require "logger"
 class SmartCloud
   include RestClient
   
+  attr_reader :instances, :volumes, :images, :addresses, :keypairs, :locations
+  
   VERSION = '20100331'
   BASE_URI = "https://www-147.ibm.com/computecloud/enterprise/api/rest/"
   
   @@last_reponse = ""
   
   def initialize(u, p, logger=Logger.new('restclient.log'))
+    
     uri = URI.join BASE_URI, VERSION
     RestClient.log = logger
-    @resource = Resource.new uri, :user => u, :password => p, :headers => { :accept => :json, :timeout => 10 }
+    
+    @resource = Resource.new uri, :user => u, :password => p, :headers => { :accept => :json}
+    
+    @instances = @resource['/instances']
+    @volumes = @resource['/volumes']
+    @images = @resource['/images']
+    @addresses = @resource['/addresses']
+    @keypairs = @resource['/keys']
+    @locations = @resource['/locations']
+    
   end
 
 # GET <base_URL>/offerings/image
 # Return the list of image offerings available to be provisioned
-  def images(image_id = '')
-    if image_id
-      response = @resource["/offerings/image/#{image_id}"].get
-    else
-      response = @resource['/offerings/image/'].get
-    end
-    @@last_reponse = response
-    images = JSON.parse(response.body)["images"]
-  end
-
-  def instances(instance_id = '')
-    if instance_id
-      response = @resource["/instances/#{instance_id}"].get
-    else
-      response = @resource['/instances'].get
-    end
-    @@last_reponse = response
-    instances = JSON.parse(response.body)["instances"]
-  end
-
-  def storage(storage_id = '')
-    if storage_id
-      response = @resource["/storage/#{storage_id}"].get
-    else
-      response = @resource["/storage"].get
-    end
-    @@last_reponse = response
-    storage = JSON.parse(response.body)["storage"]
-  end
-
-  def requests(request_id)
-    if request_id
-      response = @resource["/requests/#{request_id}"].get
-    else
-      raise "No REQUEST_ID provided"
-    end
-    @@last_reponse = response
-    requests = JSON.parse(response.body)["requests"]
-  end
-
-  def keys(key_id = '')
-    if key_id
-      response = @resource["/keys/#{key_id}"].get
-    else
-      response = @resource["/keys"].get
-    end
-    # p response
-    @@last_response = response
-    keys = JSON.parse(response.body)["keys"]
+  def describe_images
+    images.get
   end
   
-  def resource
-    @resource
+  def describe_instances
+    instances.get
+  end
+
+  def describe_volumes
+    storage.get
   end
   
-  def last_response
-    @@last_response
+  def describe_keypairs
+    keypairs.get
+  end
+  
+  def create_instance(options = {})
+    params = {
+      :name => 'test',
+      :imageID => '20025347',
+      :instanceType => 'large',
+      :publicKey => 'kraut-igm-gsg2',
+      :isMiniEphemeral => 'true'
+    }.merge!(options)
+    
+    instances.post params
   end
 
 end
